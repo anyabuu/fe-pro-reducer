eb216a8845755daa78e0e3c7212d8224e1066661
Anya Bu <annabukhtiiarova@gmail.com>
2021-12-21 14:10:34 +0200

    reducer function made

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,98 @@
+function capitalize(string) {
+
+  let arrString = string.split(' ');
+
+    function methodMap(callback) {
+    
+        let newArr = [];
+
+        for (let i = 0; i < arrString.length; i++) {
+
+            newArr.push(callback(arrString[i]));
+        }
+
+        return newArr;
+    }
+
+  let capitalizedArr = methodMap(function(word) {
+      return `${word[0].toUpperCase()}${word.slice(1)}`;
+  })
+
+  return capitalizedArr.join(' ')
+
+}
+
+
+function upperCase(string) {
+
+  let arrString = string.split(' ');
+
+    function methodMap(callback) {
+    
+      let newArr = [];
+
+      for (let i = 0; i < arrString.length; i++) {
+
+          newArr.push(callback(arrString[i]));
+      }
+
+      return newArr;
+    } 
+
+  let upperArr = methodMap(function (word) {
+      return word.toUpperCase()
+  })
+
+  return upperArr.join(' ');
+}
+
+
+
+function lowerCase(string) {
+
+  let arrString = string.split(' ');
+
+    function methodMap(callback) {
+      
+      let newArr = [];
+
+      for (let i = 0; i < arrString.length; i++) {
+
+          newArr.push(callback(arrString[i]));
+      }
+
+      return newArr;
+    } 
+
+
+  let lowerArr = methodMap(function (word) {
+      return word.toLowerCase()
+  })
+
+  return lowerArr.join(' ');
+}
+
+
 function reducer(input, action) {
-  // Ваше решение тут
+
+  switch (action) {
+    case 'capitalize':
+        return capitalize(input);
+        break;
+
+    case 'upperCase':
+        return upperCase(input);
+        break;
+
+    case 'lowerCase':
+        return lowerCase(input);
+        break;
+
+    default:
+        console.log('Error action')
+
+  }
+  
 }
 
 module.exports = reducer;
